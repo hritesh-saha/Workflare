e1b3b04249b95c0006863342aa1985fc6db35ade
Hritesh_Saha <hriteshsaha4@gmail.com>
2025-04-13 19:02:49 +0530

    completed landing page

--- job_portal/src/components/Header.jsx
+++ job_portal/src/components/Header.jsx
@@ -1,26 +1,64 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button } from './ui/button'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import React, { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
+import { Button } from "./ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+import { PenBox } from "lucide-react";
+import "../App.css";
 
 const Header = () => {
-  return (
-    <>
-    <nav className='py-4 flex justify-between items-center mr-20 ml-2'>
-        <Link>
-        <img src="/logo2.png" alt="Logo" className='h-20 cursor-pointer' />
-        </Link>
+ const [showSignIn, setShowSignIn]=useState(false);
+ const [search, setSearch]= useSearchParams();
 
-        <Button variant="outline" className="cursor-pointer">Login</Button>
-      {/* <SignedOut className="cursor-pointer">
-        <SignInButton />
-      </SignedOut>
-      <SignedIn className="cursor-pointer">
-        <UserButton />
-      </SignedIn> */}
-    </nav>
-    </>
-  )
+useEffect(()=>{
+  if(search.get('sign-in')){
+    setShowSignIn(true);
+  }
+}, [search]);
+
+const handleOverlayClick = (e)=>{
+  if(e.target === e.currentTarget){
+    setShowSignIn(false);
+    setSearch({});
+  }
 }
 
-export default Header
+  return (
+    <>
+      <nav className="py-4 flex justify-between items-center mr-20 ml-2">
+        <Link>
+          <img src="/logo2.png" alt="Logo" className="h-20 cursor-pointer" />
+        </Link>
+
+        <div className="flex gap-8">
+          <SignedOut className="cursor-pointer">
+            <Button variant="outline" onClick={() => setShowSignIn(true)}>
+              Login
+            </Button>
+          </SignedOut>
+          <SignedIn className="cursor-pointer">
+            <Link to="/post-job">
+            {/* add condition as recruiter here */}
+            <Button variant="destructive" className="rounded-full">
+              <PenBox size={20} className="mr-1"/>
+              Post a Job</Button>
+            </Link>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </nav>
+
+      {showSignIn && <div className="modal-overlay"
+      onClick={handleOverlayClick}>
+        <SignIn signUpForceRedirectUrl="/onboarding" signUpFallbackRedirectUrl="/onboarding"/>
+        </div>}
+    </>
+  );
+};
+
+export default Header;
--- job_portal/src/pages/LandingPage.jsx
+++ job_portal/src/pages/LandingPage.jsx
@@ -1,4 +1,26 @@
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 import React from "react";
+import { Link } from "react-router-dom";
+import companies from "../data/companies.json";
+import faqs from "../data/faq.json";
+import Autoplay from "embla-carousel-autoplay";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const LandingPage = () => {
   return (
@@ -6,27 +28,100 @@ const LandingPage = () => {
       <section className="text-center">
         <h1 className="flex flex-col items-center justify-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4">
           Discover Your Next Big Opportunity
-          <span className="mt-10 font-serif"
+          <span
+            className="mt-10 font-serif"
             style={{
               color: "#007bff",
-              fontWeight: "bold"
+              fontWeight: "bold",
             }}
           >
             Land the Job You Deserve!
           </span>
         </h1>
+        <p className="text-gray-300 sm:mt-4 text-xs sm:text-xl">
+          Explore thousands of job openings and find the perfect fit for your
+          skills and experience.
+        </p>
       </section>
 
-      <div>
+      <div className="flex gap-6 justify-center">
         {/* buttons */}
-        {/* carousel */}
+        <Link to="/jobs">
+          <Button variant="blue" size="xl">
+            Find Jobs
+          </Button>
+        </Link>
+        <Link to="/post-job">
+          <Button variant="destructive" size="xl">
+            Post a Job
+          </Button>
+        </Link>
       </div>
 
-      {/* banner */}
+      {/* carousel */}
+      <Carousel
+        className="w-full py-10 transition-all duration-700 ease-in-out"
+        options={{
+          loop: true,
+        }}
+        plugins={[
+          Autoplay({
+            delay: 2000,
+            stopOnInteraction: false,
+            stopOnMouseEnter: false,
+          }),
+        ]}
+      >
+        <CarouselContent className="flex gap-5 sm:gap-20 items-center">
+          {companies.map(({ name, id, path }) => {
+            return (
+              <CarouselItem key={id} className="basis-1/3 lg:basis-1/6">
+                <img
+                  src={path}
+                  alt={name}
+                  className="h-9 sm:h-14 object-contain"
+                />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
 
-      <section>{/* cards */}</section>
+      {/* banner */}
+      <img src="/jobPortalBanner.webp" alt="Banner" className="w-full h-[70vh]" />
+
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-6 mx-6">
+        {/* cards */}
+        <Card>
+          <CardHeader>
+            <CardTitle className="font-bold text-3xl">For job Seekers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Search and apply for jobs, track applications, and more.</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle className="font-bold text-3xl">For Employers</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Post jobs, manage applications, and find the best candidates.</p>
+          </CardContent>
+        </Card>
+      </section>
 
       {/* Accordian */}
+      <Accordion className="mx-6" type="single" collapsible>
+        {faqs.map((faq, index) => {
+          return (
+            <AccordionItem key={index} value={`item-${index + 1}`}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>{faq.answer}</AccordionContent>
+            </AccordionItem>
+          );
+        })}
+      </Accordion>
     </main>
   );
 };
